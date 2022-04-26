2a6bba607c5963bb98eb1ef3312225e87f5b4601
Alejandra Diaz <francys.diazurribarri2@gmail.com>
2022-04-26 10:36:11 +0200

    add new quote

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,22 @@ import '../styles/App.scss';
 
 function App() {
   const [data, setData] = useState(phrasesFriends);
+  const [newQuote, setNewQuote] = useState({
+    quote: '',
+    character: '',
+  });
+
+  const handleNewQuote = (event) => {
+    setNewQuote({
+      ...newQuote,
+      [event.target.id]: event.target.value,
+    });
+  };
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    setData([...data, newQuote]);
+  };
 
   const htmlData = data.map((character, i) => {
     return (
@@ -26,16 +42,29 @@ function App() {
         <ul>{htmlData}</ul>
         <form>
           <h2>Añade una nueva frase</h2>
-          <label htmlFor="phrase">Frase</label>
-          <input type="text" name="phrase" id="phrase" />
+          <label htmlFor="quote">Frase</label>
+          <input
+            type="text"
+            name="quote"
+            id="quote"
+            value={newQuote.quote}
+            onChange={handleNewQuote}
+          />
 
           <label htmlFor="character">Personaje</label>
-          <input type="text" name="character" id="character" />
+          <input
+            type="text"
+            name="character"
+            id="character"
+            value={newQuote.character}
+            onChange={handleNewQuote}
+          />
 
           <input
             className="new-contact__btn"
             type="submit"
             value="Añadir una nueva frase"
+            onClick={handleClick}
           />
         </form>
       </main>
